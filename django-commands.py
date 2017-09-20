a3bcdb0df6c368cbc7422a5f6f2d446130c1e020
daimon99 <daijian1@qq.com>
2017-09-20 12:33:19 +0800

    Update django-commands.py
    Don't prompt for Terminal fallback

--- django-commands.py
+++ django-commands.py
@@ -124,8 +124,8 @@ class DjangoCommand(sublime_plugin.WindowCommand):
             fallbacks = {"Linux": "xterm", "Darwin": "Terminal"}
             TERMINAL = self.settings.get(settings_names.get(PLATFORM), fallbacks.get(PLATFORM))
             if not which(TERMINAL, mode=os.F_OK | os.X_OK):
-                self.error_msg = 'terminal emulator not found: {}\nUsing fallback instead'.format(TERMINAL)
-                self.display_error_message()
+                # self.error_msg = 'terminal emulator not found: {}\nUsing fallback instead'.format(TERMINAL)
+                # self.display_error_message()
                 TERMINAL = fallbacks.get(PLATFORM)
 
     def display_error_message(self):
